7b6c02fa6cf996146390ed1bfa10f3d803530caa
Sledz <mariuszsledz89@gmail.com>
2020-01-20 19:23:24 +0100

    Add Telewizor class

---
+++ src/Telewizor.java
@@ -0,0 +1,11 @@
+public class Telewizor {
+
+    String marka;
+    int rozdzieleczonsc;
+
+    public Telewizor(String marka, int rozdzieleczonsc) {
+        this.marka = marka;
+        this.rozdzieleczonsc = rozdzieleczonsc;
+    }
+
+}
